f8f41b1d3ae95246c5d4ea30fa84110645188c0e
lucasdevsilva26 <lucasferreirasilvaadry@gmail.com>
2026-05-07 17:11:58 -0300

    update sidebar and @medias

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-setInterval(() => {[
-console.log(document.body.offsetWidth)
-]}, 50)
+setInterval(() => {
+  [console.log(document.body.offsetWidth)];
+}, 50);
+
+const menuButtons = document.querySelector("#header-buttons__menu");
+const sideBar = document.querySelector("aside");
+
+menuButtons.addEventListener("click", () => {
+  sideBar.classList.toggle('disabled')
+});
